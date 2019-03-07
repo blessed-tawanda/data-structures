8d936923b1798d8d9a69f58fd3b3cb996ad05a47
blessed-tawanda <blessedmahuni@gmail.com>
2019-03-07 16:28:32 +0200

    Added some comments to the Tower of Honai algorithm

--- Algorithms/towerofhanoi.c
+++ Algorithms/towerofhanoi.c
@@ -13,23 +13,29 @@ the to peg
 if the number of disks is 1 move it to the last peg
 if there are more disks 
 */
+int steps = 0;
 
-void tower(int a, char from, char aux, char to) {
+int tower(int a, char from, char aux, char to) {
 	if(a==1) {
 		printf("Move disc 1 from %c to %c \n", from, to);
-		return;
+		steps += 1;
+		return steps;
 	} else {
 		tower(a-1,from,to,aux);
 		printf("Move disc %d from %c to %c \n",a,from,to);
 		tower(a-1,aux,from,to);
+		steps += 1;
 	}
 }
 
 void main() {
 	int n;
 	printf("+++++++ Tower Of Hanoi +++++++\n");
+	printf("Moving From Tower A(first tower) to Tower C(third tower)\n");
+	printf("T exist enter any number and press enter");
 	printf("Enter number of discs: ");
 	scanf("%d",&n);
-	tower(n,'A','B','C');
+	int steps = tower(n,'A','B','C');
+	printf("\nNumber of Steps: %d", steps);
 	scanf("%d");
 }
